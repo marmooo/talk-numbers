4a171f1449f009c35771445d01a06c3725a406e9
marmooo <marmooo@users.noreply.github.com>
2022-11-26 13:00:49 +0900

    bump bootstrap from 5.2.2 to 5.2.3

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = "2022-11-04 09:30";
+var CACHE_NAME = "2022-11-26 12:58";
 var urlsToCache = [
   "/talk-numbers/",
   "/talk-numbers/kohacu.webp",
@@ -7,7 +7,7 @@ var urlsToCache = [
   "/talk-numbers/mp3/cat.mp3",
   "/talk-numbers/mp3/correct3.mp3",
   "/talk-numbers/favicon/favicon.svg",
-  "https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css",
 ];
 
 self.addEventListener("install", function (event) {
